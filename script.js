a555c1a78e4fa5132f73c356b7736b7af4e060f5
Maximilian Weber <maximilianweber@MBPvonMximilian.fritz.box>
2022-09-06 12:51:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+let priceListEins = [
+    {
+        "name": "Polster abnehmbar",
+        "price": "CHF 15",
+        "icon": "./img/polsterAbnehmbar.png",
+    },
+    {
+        "name": "Hocker",
+        "price": "CHF 25",
+        "icon": "./img/hocker.png",
+    },
+    {
+        "name": "Sessel",
+        "price": "CHF 69",
+        "icon": "./img/sessel.png",
+    },
+    {
+        "name": "Sofa 2-Sitzer",
+        "price": "CHF 139",
+        "icon": "./img/sofaZwei.png",
+    },
+    {
+        "name": "Sofa 3-Sitzer",
+        "price": "CHF 199",
+        "icon": "./img/sofaDrei.png",
+    },
+    {
+        "name": "Eckcouch klein",
+        "price": "CHF 275",
+        "icon": "./img/polsterreinigung.png",
+    },
+    {
+        "name": "Eckcouch groß",
+        "price": "CHF 329",
+        "icon": "./img/eckcouchgross.png",
+    },
+    {
+        "name": "Kombination auf Anfrage",
+        "price": "",
+        "icon": "./img/polsterAbnehmbar.png",
+
+    },
+
+];
+
+/*function init() {
+    render();
+}
+
+function render() {
+    renderPriceListEins();
+}
+
+function renderPriceListEins() {
+    let priceList = document.getElementById('priceListEins');
+
+    for (let i = 0; i < priceListEins.length; i++) {
+        priceList.innerHTML += creatPriceListEins(i);
+    }
+}
+
+function creatPriceListEins(i){
+    return`
+    <div class="priceListChild">
+        <table>
+            ${priceListEins[i]['name']}
+            ${priceListEins[i]['price']}
+            ${priceListEins[i]['name']}
+        </table>
+    </div>
+    `
+    ;
+}
+*/
